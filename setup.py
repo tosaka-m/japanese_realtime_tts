ff4037c68a487771927c54ae8b5f72b0a45f8217
tosaka <aaa>
2020-08-06 19:44:15 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,9 @@
 from setuptools import setup
-setup()
+from Cython.Build import cythonize
+import numpy
+
+setup(
+    ext_modules = cythonize('src/jrtts/GlowTTS/Networks/monotonic_align/core.pyx'),
+    include_dirs=[numpy.get_include()]
+)
+
